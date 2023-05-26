0cfa75484ab75736242762277df79c9384822cbb
tommytuo@apjcorp.com <12345678>
2023-05-26 17:50:04 +0800

    ++

--- 自己写/rn_demo/App.js
+++ 自己写/rn_demo/App.js
@@ -1,4 +1,50 @@
-import Index from "./screen/04/07";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import HomeScreen from "./screen/05/03/HomeScreen";
+import DetailScreen from "./screen/05/03/DetailScreen";
+import SettingScreen from "./screen/05/03/SettingScreen";
+import ProfileScreen from "./screen/05/03/ProfileScreen";
+
+// 创建一个栈导航器
+const HomeStack = createNativeStackNavigator();
+const SettingStack = createNativeStackNavigator();
+
+// 创建一个标签页导航
+const Tab = createBottomTabNavigator();
+
 export default function App() {
-  return <Index />;
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        {/* 第一个标签页 */}
+        <Tab.Screen name="First">
+          {/* 里面嵌套一个栈导航器 */}
+          {() => (
+            <HomeStack.Navigator>
+              <HomeStack.Screen name="HomeScreen" component={HomeScreen} />
+              <HomeStack.Screen name="DetailScreen" component={DetailScreen} />
+            </HomeStack.Navigator>
+          )}
+        </Tab.Screen>
+        {/* 第二个标签页 */}
+        <Tab.Screen name="Second">
+          {/* 里面嵌套一个栈导航器 */}
+          {() => (
+            <SettingStack.Navigator>
+              <SettingStack.Screen
+                name="SettingScreen"
+                component={SettingScreen}
+              />
+              <SettingStack.Screen
+                name="ProfileScreen"
+                component={ProfileScreen}
+              />
+            </SettingStack.Navigator>
+          )}
+        </Tab.Screen>
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
